06aa68afbeeca214a413547c06cab5f8269360a5
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2013-03-02 19:13:45 +0100

    Allow to specify php version for which will be installed extensions

--- extensions/compile-all.sh
+++ extensions/compile-all.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+version=$1
+phpize_location="../../inst/bin/phpize-${version}"
+php_config="../../inst/bin/php-config-${version}"
+
+if [[ ${version} == '' ]]; then
+    phpize_location='phpize'
+    php_config='php-config'
+fi
+
 extensions=$(ls -p | grep '/')
 current_dir=$(pwd)
 
 for extension in $extensions; do
     cd "${current_dir}/${extension}"
-    make clean && phpize && ./configure && make && make install
+    make clean && ${phpize_location} && ./configure --with-php-config=${php_config} && make && make install
 done
 
